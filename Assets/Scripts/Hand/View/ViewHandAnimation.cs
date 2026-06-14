d1d6d69f0787a1bbc8e548f930e49473deae4d41
Ashkan <1hajy.ashkan@gmail.com>
2026-06-14 13:45:23 +0330

    Character Movement // Grab System // Character hand model // Grab and pointing animation // Customize teleportation

---
+++ Assets/Scripts/Hand/View/ViewHandAnimation.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.XR.Interaction.Toolkit;
+
+
+public class ViewHandAnimation : MonoBehaviour
+{
+    [SerializeField]Animator handAnimator;
+    private const string GRAB="Grab";
+    private const string POINT="Point";
+    public void OnGrab(bool State)
+    {
+        handAnimator.SetBool(GRAB,State);
+    }
+    public void OnPint(bool State)
+    {
+        handAnimator.SetBool(POINT,State);
+    }
+}
